48da0f9aac88045e553002b150f7b36d53556a96
Roberto <roberto.guarneros11@gmail.com>
2022-10-09 19:46:51 +0200

    Se modifica el cliente para poder conectarse y desconectarse

--- clienteEjercicioGuia/WindowsFormsApplication1/Form1.Designer.cs
+++ clienteEjercicioGuia/WindowsFormsApplication1/Form1.Designer.cs
@@ -34,6 +34,11 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.Longitud = new System.Windows.Forms.RadioButton();
             this.Bonito = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Altura = new System.Windows.Forms.RadioButton();
+            this.AlturaBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -41,7 +46,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(23, 25);
+            this.label2.Location = new System.Drawing.Point(33, 72);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(87, 25);
             this.label2.TabIndex = 1;
@@ -49,14 +54,14 @@
             // 
             // nombre
             // 
-            this.nombre.Location = new System.Drawing.Point(116, 31);
+            this.nombre.Location = new System.Drawing.Point(126, 78);
             this.nombre.Name = "nombre";
             this.nombre.Size = new System.Drawing.Size(164, 20);
             this.nombre.TabIndex = 3;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(130, 144);
+            this.button2.Location = new System.Drawing.Point(140, 191);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
@@ -67,6 +72,11 @@
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.AlturaBox);
+            this.groupBox1.Controls.Add(this.Altura);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.Longitud);
             this.groupBox1.Controls.Add(this.Bonito);
             this.groupBox1.Controls.Add(this.label2);
@@ -82,7 +92,7 @@
             // Longitud
             // 
             this.Longitud.AutoSize = true;
-            this.Longitud.Location = new System.Drawing.Point(116, 91);
+            this.Longitud.Location = new System.Drawing.Point(126, 138);
             this.Longitud.Name = "Longitud";
             this.Longitud.Size = new System.Drawing.Size(166, 17);
             this.Longitud.TabIndex = 7;
@@ -93,13 +103,61 @@
             // Bonito
             // 
             this.Bonito.AutoSize = true;
-            this.Bonito.Location = new System.Drawing.Point(116, 68);
+            this.Bonito.Location = new System.Drawing.Point(126, 115);
             this.Bonito.Name = "Bonito";
             this.Bonito.Size = new System.Drawing.Size(156, 17);
             this.Bonito.TabIndex = 8;
             this.Bonito.TabStop = true;
             this.Bonito.Text = "Dime si mi nombre es bonito";
             this.Bonito.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(45, 231);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Desconectar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Altura
+            // 
+            this.Altura.AutoSize = true;
+            this.Altura.Location = new System.Drawing.Point(124, 161);
+            this.Altura.Name = "Altura";
+            this.Altura.Size = new System.Drawing.Size(98, 17);
+            this.Altura.TabIndex = 11;
+            this.Altura.TabStop = true;
+            this.Altura.Text = "Dime si soy alto";
+            this.Altura.UseVisualStyleBackColor = true;
+            // 
+            // AlturaBox
+            // 
+            this.AlturaBox.Location = new System.Drawing.Point(38, 161);
+            this.AlturaBox.Name = "AlturaBox";
+            this.AlturaBox.Size = new System.Drawing.Size(66, 20);
+            this.AlturaBox.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Altura:";
             // 
             // Form1
             // 
@@ -124,6 +182,11 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton Longitud;
         private System.Windows.Forms.RadioButton Bonito;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox AlturaBox;
+        private System.Windows.Forms.RadioButton Altura;
     }
 }
 
--- clienteEjercicioGuia/WindowsFormsApplication1/Form1.cs
+++ clienteEjercicioGuia/WindowsFormsApplication1/Form1.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
@@ -21,90 +17,95 @@ namespace WindowsFormsApplication1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
-           
         }
 
-   
-        private void button2_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
 
             //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
             //al que deseamos conectarnos
             IPAddress direc = IPAddress.Parse("192.168.56.102");
-            IPEndPoint ipep = new IPEndPoint(direc, 9050);
-
-
+            IPEndPoint ipep = new IPEndPoint(direc, 9080);
             //Creamos el socket 
             server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             try
             {
                 server.Connect(ipep);//Intentamos conectar el socket
                 this.BackColor = Color.Green;
-             
-
-                if (Longitud.Checked)
-                {
-                    // Quiere saber la longitud
-                    string mensaje = "1/" + nombre.Text;
-                    // Enviamos al servidor el nombre tecleado
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
-                    server.Send(msg);
-
-                    //Recibimos la respuesta del servidor
-                    byte[] msg2 = new byte[80];
-                    server.Receive(msg2);
-                    mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
-                    MessageBox.Show("La longitud de tu nombre es: " + mensaje);
-                }
-                else
-                {
-                    // Quiere saber si el nombre es bonito
-                    string mensaje = "2/" + nombre.Text;
-                    // Enviamos al servidor el nombre tecleado
-                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
-                    server.Send(msg);
-
-                    //Recibimos la respuesta del servidor
-                    byte[] msg2 = new byte[80];
-                    server.Receive(msg2);
-                    mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
-
-
-                    if (mensaje == "SI")
-                        MessageBox.Show("Tu nombre ES bonito.");
-                    else
-                        MessageBox.Show("Tu nombre NO es bonito. Lo siento.");
-
-
-                }
-             
-                // Se terminó el servicio. 
-                // Nos desconectamos
-                this.BackColor = Color.Gray;
-                server.Shutdown(SocketShutdown.Both);
-                server.Close();
-
-
-
+                MessageBox.Show("Conectado");
             }
-            catch (SocketException )
+            catch (SocketException)
             {
                 //Si hay excepcion imprimimos error y salimos del programa con return 
                 MessageBox.Show("No he podido conectar con el servidor");
                 return;
-            } 
-
-          
-
-    
-          
-          
+            }
 
         }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Longitud.Checked)
+            {
+                // Quiere saber la longitud
+                string mensaje = "1/" + nombre.Text;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
 
-   
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                MessageBox.Show("La longitud de tu nombre es: " + mensaje);
+            }
+            else if (Bonito.Checked)
+            {
+                // Quiere saber si el nombre es bonito
+                string mensaje = "2/" + nombre.Text;
+                // Enviamos al servidor el nombre tecleado
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
 
-     
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+
+
+                if (mensaje == "SI")
+                    MessageBox.Show("Tu nombre ES bonito.");
+                else
+                    MessageBox.Show("Tu nombre NO es bonito. Lo siento.");
+
+            }
+            else
+            {
+                //Enviamos el nombre y la altura
+                string mensaje = "3/" + nombre.Text + "/" + AlturaBox.Text;
+                //Enviamos al servidor el nombre
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+
+                //Recibimos la respuesta 
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                MessageBox.Show(mensaje);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Mensaje desconexion
+            string mensaje = "0/";
+
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+            // Nos desconectamos
+            this.BackColor = Color.Gray;
+            server.Shutdown(SocketShutdown.Both);
+            server.Close();
+        }
     }
 }
